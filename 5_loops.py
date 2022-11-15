e7eaea77209d43373aaf75ab15af4632174ebf6b
manotik12 <manotik254@gmail.com>
2022-11-15 13:52:27 +0300

    new files

---
+++ 5_loops.py
@@ -0,0 +1,11 @@
+# the keyword use is "for"
+my_fruits = ["mango", "orange", "peach", "apple"]
+
+for fruit in my_fruits:  # fruit is an itterator created automatically by python
+    print(fruit)
+# len is a function to get the length
+print(f"the list{my_fruits} has {len(my_fruits)} items")
+
+my_name = "jonathan"
+for i in my_name:
+    print(i)
---
+++ studentMarks.py
@@ -0,0 +1,12 @@
+student_mark = int(input("enter final mark"))
+
+if (student_mark >= 70):
+    print("pass: Grade A")
+elif (student_mark >= 60):
+    print("pass: Grade B")
+elif (student_mark >= 50):
+    print("pass: Grade C")
+elif (student_mark >= 40):
+    print("pass: Grade D")
+else:
+    print("fail")
